73c23789397def54754637cae24aaf22eea864ad
Evgeny Agafonov <eagafonov@spbdev.ru>
2019-06-08 21:55:18 +0300

    first

---
+++ precache-manifest.a47482ef277ecc3dcf8a36fc7184300a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34c6b6abebe102e6dbfb",
+    "url": "/static/css/main.80d31c03.chunk.css"
+  },
+  {
+    "revision": "34c6b6abebe102e6dbfb",
+    "url": "/static/js/main.34c6b6ab.chunk.js"
+  },
+  {
+    "revision": "66d77e0d37e923d2ef86",
+    "url": "/static/js/1.66d77e0d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a44e13f386cd310318869c53f5467fa",
+    "url": "/index.html"
+  }
+];
